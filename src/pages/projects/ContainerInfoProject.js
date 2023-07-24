ccabfdf21536aafd04a4e55c15dfbd306f21295c
Vinicius <karvvinicius@gmail.com>
2023-07-24 19:43:59 +0100

    modific Page Projects

--- src/pages/projects/ContainerInfoProject.js
+++ src/pages/projects/ContainerInfoProject.js
@@ -1,13 +1,60 @@
 import '../../components/master.css';
-import astronauta from '../../components/Astronauta.png';
+import jurosCompostos from '../../components/img/juros-compostos.png';
 
 function ProjectsContainer() {
   return (
     <div className="container" style={styles.containerInfo}>
-        <div id='InfoContainer' style={styles.Info}>
-            <h1 style={styles.h1}>I'M VINICIUS<br/>CARVALHO</h1>
-            <img className='astronauta' src={astronauta} alt="icon from github" />  
+        <div style={styles.Info}>
+          <div className="d-flex CampoMyPortif" >
+            <h1 style={styles.h1}>My<br/>portfolio</h1>
+            <p className="PInfo" style={styles.p}>These are some of the jobs and<br/>projects I've done</p>
+          </div>
+          <ul className="nav nav-pills" id="pills-tab" role="tablist">
+            <li className="nav-item" role="presentation">
+              <button className="navEdit active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Todos</button>
+            </li>
+            <li className="nav-item" role="presentation">
+              <button className="navEdit " id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">My Favorites</button>
+            </li>
+            <li className="nav-item" role="presentation">
+              <button className="navEdit" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Active</button>
+            </li>
+          </ul>
+          <div className="tab-content" id="pills-tabContent">
+            <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
+              <div className="container text-center">
+                <div className="row">  
+                      <div className="col-12 col-md-6 col-lg-4">
+                        <div class="card">
+                          <img src={jurosCompostos} class="card-img-top" alt="..."/>
+                          <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          </div>
+                        </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">
+                <div className="container text-center">
+                  <div className="row">
+                    <div>
+                      <p>Favorites</p>
+                    </div>
+                  </div>                    
+                </div>
+            </div>
+            <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab" tabindex="0">
+                <div className="container text-center">
+                    <div className="row">
+                        <div>
+                            <p>none</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
     </div>
   );
 }
@@ -24,11 +71,14 @@ const styles = {
     backgroundColor:' rgba(181, 180, 180, 0.226)',
     padding: '30px',
     fontFamily: "'Lora', serif",
-    textAlign:'center',
   },
   h1:{
     fontSize: '50px',
   },
+  p:{
+    marginTop:'auto',
+    marginLeft:'30px',
+  }
 
 };
 
