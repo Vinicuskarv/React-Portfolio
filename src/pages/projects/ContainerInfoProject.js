f69287b3f17faf0782710928eeed662f7b4269e1
Vinicius <karvvinicius@gmail.com>
2023-07-30 18:53:03 +0100

    modify links

--- src/pages/projects/ContainerInfoProject.js
+++ src/pages/projects/ContainerInfoProject.js
@@ -16,10 +16,6 @@ import iconCSS from '../../components/icons/icons8-css-50.png';
 import iconJS from '../../components/icons/icons8-js-50.png';
 import iconPHP from '../../components/icons/icons8-php-50.png';
 
-
-
-
-
 function ProjectsContainer() {
   return (
     <div className="container" style={styles.containerInfo}>
@@ -213,12 +209,12 @@ function ProjectsContainer() {
                         <div className="campoIconstool">
                           <img src={iconHTML} alt="html"/>
                           <img src={iconCSS} alt="css" />
-                          <img src={iconJS} alt="js"/>                                          
+                          <img src={iconJS} alt="js"/>
                         </div>
                       </div>
                     </div>
                   </div>
-                  </div>                    
+                  </div>
                 </div>
             </div>
             <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab" tabIndex="0">
